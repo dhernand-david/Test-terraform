11a98acdfdfa4f2e484341c69db3741a6fd651c8
Hernandez Rosa <david.hernandez-rosa@capgemini.com>
2021-09-29 11:38:53 +0200

    v009

--- Plantillas/main-github-lab.tf
+++ Plantillas/main-github-lab.tf
@@ -13,10 +13,36 @@ provider "azurerm" {
   features {}
  }
 
-resource "azurerm_resource_group" "example" {
-  name     = "rg_git"
+resource "azurerm_resource_group" "rg" {
+  name     = "rg_app-devops"
   location = "West Europe"
 }
 
+// App Service Plan
+resource "azurerm_app_service_plan" "service-plan" {
+  name                = "asp-dhernand-terraform"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  kind = "Linux"
+  reserved = true
 
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+// App Service
+resource "azurerm_app_service" "appsvc" {
+  name                = "app-dhernand-terraform"
+  location            = "West Europe"
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+  https_only = true
+
+  site_config {
+      ftps_state = "FtpsOnly"
+      always_on = true
+  }
+}
 
