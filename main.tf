ac85e9e5419f325948aa781e6add39a6ba10ec5c
dhernand-david <91538984+dhernand-david@users.noreply.github.com>
2022-03-10 21:52:34 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -9,11 +9,16 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-
+  subscription_id = "f316307d-3970-4d1f-9a71-28ab4dec62ab"
+  client_id       = "4543c90a-9e9e-43a1-a45b-41d7e28ce141"
+  client_secret   = "aylyhurFfE.VGriYPa08l4U9f-JNdGgfci"
+  tenant_id       = "111d9840-cadf-4780-8b46-07fa01ce4830"
   features {}
- }
+  
+}
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg_app-devops"
+// Resource Group
+resource "azurerm_resource_group" "resource-group" {
+  name     = "dhernand_terraform22"
   location = "West Europe"
 }
